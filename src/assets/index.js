8606e5c5e4a7863727b08934557e5b0c1cc667cc
AmrBenshi <amrsaeedbenshi@gmail.com>
2024-08-02 22:14:46 +0300

    ADD

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,11 +21,9 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
+import neon from "./company/neon.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
@@ -52,10 +50,9 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
+  neon,
   shopify,
   starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
